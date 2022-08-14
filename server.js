51506ddfe2849414f550984e954c7df82b080055
Karlo <kvukob@gmail.com>
2022-08-13 20:23:51 -0400

    Heroku config

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(express.static(__dirname + "/dist/"));
 app.use(cors());
 app.get(/.*/, function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
   res.sendfile(__dirname + "/dist/index.html");
 });
 app.listen(port);

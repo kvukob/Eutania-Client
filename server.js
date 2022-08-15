f8283c9d9ba8849e55c7675d0205bae27c927402
Karlo <kvukob@gmail.com>
2022-08-15 18:43:25 -0400

    aas

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const port = process.env.PORT || 8080;
+const cors = require("cors");
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+app.use(cors());
+app.get(/.*/, function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  res.sendfile(__dirname + "/dist/index.html");
+});
+app.listen(port);
+
+console.log("started");

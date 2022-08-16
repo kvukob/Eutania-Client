d8370ee8308e4ee8e5684eee40230f5758aa6360
Karlo <kvukob@gmail.com>
2022-08-15 21:33:37 -0400

    sdf

--- server.js
+++ server.js
@@ -1,20 +1,7 @@
 const express = require("express");
-const port = process.env.PORT || 8080;
-const cors = require("cors");
-const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/dist/"));
-app.use(cors());
-app.get(/.*/, function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  res.sendfile(__dirname + "/dist/index.html");
-});
-app.listen(port);
-
-console.log("startedok");
+express()
+  .use(express.static(path.join(__dirname, "/dist/")))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
